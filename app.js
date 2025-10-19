a14c542aa83f0841da082f87ed4504365a9e9e61
lufegoro78@gmail.com <davxpa.02@gmail.com>
2025-10-18 20:20:40 -0500

    hecho en clase

---
+++ app.js
@@ -0,0 +1,43 @@
+// let numero, contador, resultado;
+// numero=parseInt(prompt(`ingrese un numero: `));
+// if(!isNaN(numero)){
+//     for (let i=1; i<=9; i++){
+//     resultado= numero*i;
+// if(resultado % 2===0){
+// console.log(`par ${numero} * ${i} = ${resultado}`);
+// }
+// else if(resultado % 2!=0){
+    // console.log(`impar ${numero} * ${i} = ${resultado}`);
+// }
+
+// }
+// }else{console.log(`no es un numero`)};
+// let numero, primo;
+
+// for (let numero=2; numero<=100; numero++){ 
+//     primo = true;
+// for(let i=2; i<numero; i++){
+//     if(numero % i === 0){
+//         primo=false;
+    
+        
+//     }
+// }
+// if(primo){
+//     console.log(numero);
+    
+// }
+    
+// }
+//un programa que cada que reciva un numero me de como resultado su raiz, y que no reciva letras
+while(true){
+
+
+
+
+}
+
+
+
+
+
